5146980a6498a512eef599eb2b203b3b332e8c9d
danieloi <mayowadan17@gmail.com>
2019-04-16 13:38:09 +0100

    update author information

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
     title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
+    author: `Mayowa Daniel`,
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `maydan2a`,
     },
   },
   plugins: [
